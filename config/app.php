a815463544358c043db9ad850b39c5f5bf4fdc97
gustopa <gustopaMI@gmail.com>
2021-05-19 23:08:47 +0700

    Fix bugs

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://koperasi-spa.herokuapp.com/'),
+    'url' => 'https://koperasi-spa.herokuapp.com/',
 
     'asset_url' => env('ASSET_URL', null),
 
